51fa1dbde84c3383290cab41d857a46db088d5c8
Abdul Moiz <abdul.m01z10123@gmail.com>
2025-03-11 21:23:32 +0000

    Imported Image tag

--- src/components/CharacterCard.tsx
+++ src/components/CharacterCard.tsx
@@ -1,8 +1,10 @@
+import Image from 'next/image';
+
 export default function CharacterCards() {
   return (
     <div className="card bg-base-100 w-96 shadow-sm">
       <figure className="px-10 pt-10">
-        <img
+        <Image
           src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
           alt="Shoes"
           className="rounded-xl"
