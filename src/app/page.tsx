72e7a2d4ff99cf45533a9825425a9e6cd46361cb
Abdul Moiz <abdul.m01z10123@gmail.com>
2025-03-11 20:48:41 +0000

    Added a subtitle comment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ const Home = () => {
     <div>
       {/* Title */}
       <h1>Welcome to OPBR Companion</h1>
+      {/* Sub title */}
       <h3>YOUR guide to reach the top leagues</h3>
     </div>
   );
