e15a727e112210454fd73134721754a2f4fe3b2f
Abdul Moiz <104764143+A-Moiz@users.noreply.github.com>
2025-03-11 20:50:41 +0000

    Revert "Added title comment"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 const Home = () => {
   return (
     <div>
-      {/* Title */}
       <h1>Welcome to OPBR Companion</h1>
-      {/* Sub title */}
       <h3>YOUR guide to reach the top leagues</h3>
     </div>
   );
