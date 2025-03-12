2702df7f26546f3f24454df97a3f86573b956fec
Mohamed Yusuf Mohamed <107251702+Mohamed-Y-Mohamed@users.noreply.github.com>
2025-03-12 02:01:03 +0000

    changes implemented to fronted component and global variable initialise for dark mode

--- src/app/characters/page.tsx
+++ src/app/characters/page.tsx
@@ -1,9 +1,26 @@
-import CharacterCards from '@/components/CharacterCard';
+'use client';
+import CharacterCards from '../../components/CharacterCard';
+
 const Characters = () => {
+  const characters = [
+    { name: 'Luffy', image: 'https://drive.google.com/uc?export=view&id=16ADdwgQ1sf4Mn-6oBLBNU2Sk5aFBoJYw' },
+    { name: 'Zoro', image: 'https://drive.google.com/uc?export=view&id=1YVgG-RPXkLf3IxfGY0ECSDQRUBUoD7OL' },
+    { name: 'Sanji', image: 'https://drive.google.com/uc?export=view&id=19sMwYevl8f5pCBSUiFYZlSNG_PBCv25I' },
+    { name: 'Shanks', image: 'https://drive.google.com/uc?export=view&id=1BJ4qx5WCC_pi-mUw4Aj4UE6sdbS49rTp' },
+    { name: 'Kalgara', image: 'https://drive.google.com/uc?export=view&id=1MW_SearF1i17s7poLVrYFVqQhS5_eKnd' },
+    { name: 'Garp', image: 'https://drive.google.com/uc?export=view&id=1Hv5rfQm0lP5FF59LjAbTozuRH4XdLjrR' }
+  ];
+
   return (
-    <div>
-      Characters
-      <CharacterCards />
+    <div className="min-h-screen p-6">
+      <h1 className="text-2xl font-bold text-center mb-6">Character Gallery</h1>
+      <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-4 pb-16">
+        {characters.map((char, idx) => (
+          <div key={idx} className="flex justify-center p-0">
+            <CharacterCards name={char.name} image={char.image} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
