defa1fee49e779bde55b63b03215825559c582a4
Abdul Moiz <abdul.m01z10123@gmail.com>
2025-03-12 18:28:49 +0000

    Fetching and displaying data from medal set table

--- src/components/MedalSetCard.tsx
+++ src/components/MedalSetCard.tsx
@@ -1,42 +1,26 @@
 import Image from 'next/image';
 import Logo from '../../public/images/logo.png';
 
-const MedalSetCard = () => {
+interface MedalSetCardProps {
+  medals: string[]; // Array of image URLs
+  medalTraits: string[]; // Array of trait tags
+}
+
+const MedalSetCard: React.FC<MedalSetCardProps> = ({ medals, medalTraits }) => {
   return (
-    <div>
-      <ul className="list bg-base-100 rounded-box shadow-md">
-        <li className="p-4 pb-2 text-xs opacity-60 tracking-wide">Most played songs this week</li>
-
-        <li className="list-row">
-          <div>
-            <Image className="size-10 rounded-box" src={Logo} alt="image 1" width={100} height={100} />
-          </div>
-          <div>
-            <div>Dio Lupa</div>
-            <div className="text-xs uppercase font-semibold opacity-60">Remaining Reason</div>
-          </div>
-        </li>
-
-        <li className="list-row">
-          <div>
-            <Image className="size-10 rounded-box" src={Logo} alt="image 2" width={100} height={100} />
-          </div>
-          <div>
-            <div>Ellie Beilish</div>
-            <div className="text-xs uppercase font-semibold opacity-60">Bears of a fever</div>
-          </div>
-        </li>
-
-        <li className="list-row">
-          <div>
-            <Image className="size-10 rounded-box" src={Logo} alt="image 3" width={100} height={100} />
-          </div>
-          <div>
-            <div>Sabrino Gardener</div>
-            <div className="text-xs uppercase font-semibold opacity-60">Cappuccino</div>
-          </div>
-        </li>
-      </ul>
+    <div className="p-4 bg-base-100 rounded-lg shadow-md">
+      <div className="flex gap-2">
+        {medals.map((img, idx) => (
+          <img key={idx} src={img} alt={`Medal ${idx + 1}`} className="w-20 h-20 rounded-md" />
+        ))}
+      </div>
+      <div className="mt-2 text-sm text-gray-600">
+        {medalTraits.map((trait, idx) => (
+          <span key={idx} className="bg-gray-200 text-gray-800 px-2 py-1 rounded-md text-xs mr-2">
+            {trait}
+          </span>
+        ))}
+      </div>
     </div>
   );
 };
