f985a66dfcda68d1d2239c9141b3ecdff6bd7da1
Mohamed Yusuf Mohamed <107251702+Mohamed-Y-Mohamed@users.noreply.github.com>
2025-03-13 01:36:52 +0000

    minor fixing for medal set card

--- src/app/medal-sets/page.tsx
+++ src/app/medal-sets/page.tsx
@@ -28,9 +28,9 @@ const MedalSets = () => {
   }, []);
 
   return (
-    <div className="p-6">
-      <h2 className="text-2xl font-bold mb-4">Medal Sets</h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+    <div className="p-6 min-h-screen">
+      <h2 className="text-2xl font-bold mb-4 ">Medal Sets</h2>
+      <div className="grid grid-cols-1  md:grid-cols-2 lg:grid-cols-3 gap-4">
         {medalSets.map((medalSet, index) => (
           <MedalSetCard key={index} medals={medalSet.medals} medalTraits={medalSet.medal_traits} />
         ))}
