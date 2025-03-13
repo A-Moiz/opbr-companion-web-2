4d179a9ab8a0a2516209ef6b235f71c8ed6e3cb7
Mohamed Yusuf Mohamed <107251702+Mohamed-Y-Mohamed@users.noreply.github.com>
2025-03-13 08:09:34 +0000

    added pop up feature

--- src/components/CharacterCard.tsx
+++ src/components/CharacterCard.tsx
@@ -8,7 +8,7 @@ interface CharacterCardProps {
 export default function CharacterCards({ name, image }: CharacterCardProps) {
   return (
     <div
-      className={`w-60 h-60 p-4 rounded-xl shadow-md transition-colors flex flex-col items-center ${'bg-gray-200 text-black'}`}
+      className={`w-60 h-60 p-4 rounded-xl shadow-md transition-colors flex flex-col items-center bg-base-100 text-white`}
     >
       <figure className="flex justify-center pt-2">
         <Image src={image} alt={name} className="rounded-2xl" width={175} height={175} />
