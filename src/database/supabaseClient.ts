af7246ebfbe4a07a88369e6a67a1c63afd96c1f4
dominik <105545749+dominik-deak@users.noreply.github.com>
2025-03-16 18:24:54 +0000

    fix double supabase export

--- src/database/supabaseClient.ts
+++ src/database/supabaseClient.ts
@@ -6,13 +6,11 @@ const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 if (!SUPABASE_DOMAIN) {
   throw new Error('Missing NEXT_PUBLIC_SUPABASE_DOMAIN. Please set it in .env.local file.');
 }
-
 if (!SUPABASE_ANON_KEY) {
   throw new Error('Missing NEXT_PUBLIC_SUPABASE_ANON_KEY. Please set it in .env.local file.');
 }
 
 const SUPABASE_URL = `https://${SUPABASE_DOMAIN}`;
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 export default supabase;
