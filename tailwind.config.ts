87eab2c4cc8f39c0077df8ef2cb2658d6123ccd9
Abdul Moiz <abdul.m01z10123@gmail.com>
2025-03-19 20:13:24 +0000

    Created tailwind config file

---
+++ tailwind.config.ts
@@ -0,0 +1,20 @@
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  darkMode: 'class',
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        coral: '#FF7F50',
+        scarlet: '#FF2400'
+      }
+    }
+  },
+  plugins: []
+};
+export default config;
